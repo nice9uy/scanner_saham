9c16f7841db2be5dfb5eb5bdeaa0af6c5d1092a2
nice9uy <abdulrachman.mails@gmail.com>
2024-10-04 18:31:29 +0700

    fix: Error jika tabel berada di halaman lain

--- static/js/custom_js/all_data.js
+++ static/js/custom_js/all_data.js
@@ -1,11 +1,29 @@
-$(document).ready(function() {
-    var table = new Tabulator("#table_settings", {
-        ajaxURL: '/scanner/setting_data/',
-        autoColumns: true,
-        ajaxError: function(err) {
-            console.error("Error fetching data: ", err);
-        }
-    });
-});
+// $(document).ready(function() {
+//     if ($("#table_settings").length > 0) {
+        
+//         var table = new Tabulator("#table_settings", {
+//             ajaxURL: '/scanner/setting_data/',
+//             autoColumns: true,
+//             ajaxError: function(err) {
+//                 console.error("Error fetching data: ", err);
+//             }
+//         });
 
+//     } else {
+//         console.log("Table element not found on this page.");
+//     }
+
+// });
+
+$(document).ready(function() {
+    if ($("#table_settings").length) {
+        var table = new Tabulator("#table_settings", {
+            ajaxURL: "/scanner/setting_data/",
+            autoColumns: true,
+            ajaxError: function(err) {
+                console.error("Error fetching data: ", err);
+            }
+        });
+    }
+});
 
