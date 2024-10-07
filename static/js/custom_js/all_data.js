6c09909f07da84892ee72913aa06284f6611e5bf
nice9uy <abdulrachman.mails@gmail.com>
2024-10-07 19:42:17 +0700

    Menambahkan fitur di setting

--- static/js/custom_js/all_data.js
+++ static/js/custom_js/all_data.js
@@ -1,29 +1,36 @@
-// $(document).ready(function() {
-//     if ($("#table_settings").length > 0) {
-        
-//         var table = new Tabulator("#table_settings", {
-//             ajaxURL: '/scanner/setting_data/',
-//             autoColumns: true,
-//             ajaxError: function(err) {
-//                 console.error("Error fetching data: ", err);
-//             }
-//         });
-
-//     } else {
-//         console.log("Table element not found on this page.");
-//     }
-
-// });
-
 $(document).ready(function() {
     if ($("#table_settings").length) {
+        var counter = 1; // Inisialisasi counter dari 1
         var table = new Tabulator("#table_settings", {
             ajaxURL: "/scanner/setting_data/",
-            autoColumns: true,
+            autoColumns: false,
+            layout:"fitColumns",
+            pagination:"local",
+            paginationSize:15,
+            paginationSizeSelector:[10, 15, 30, 50],
+            columns: [
+                {
+                    title: "No",
+                    field: "no",
+                    formatter: function(cell) {
+                        return counter++; // Kembalikan nilai counter dan increment
+                    },
+                    hozAlign: "center", // Menyelaraskan teks ke kanan
+                    width: 150 ,// Lebar kolom,
+                    headerHozAlign: "center", // Menyelaraskan teks judul kolom ke tengah
+                },        
+                {title: "COMPANY NAME", field: "company_name" ,  headerHozAlign: "center",  },
+                {title: "LISTING BOARD", field: "listing_board",  headerHozAlign: "center", },
+            ],
             ajaxError: function(err) {
                 console.error("Error fetching data: ", err);
             }
         });
+        $("#search").on("keyup", function() {
+            const searchTerm = $(this).val();
+            table.setFilter("company_name", "like", searchTerm); // Filter berdasarkan kolom 'name'
+        });
+        
     }
 });
 
